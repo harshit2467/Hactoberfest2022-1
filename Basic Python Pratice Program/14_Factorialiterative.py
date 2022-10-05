51794855ac2b4b20334b74e68f32e54beac8182f
harshit2467 <92702495+harshit2467@users.noreply.github.com>
2022-10-05 09:34:55 +0530

    Update 14_Factorialiterative.py

--- Basic Python Pratice Program/14_Factorialiterative.py
+++ Basic Python Pratice Program/14_Factorialiterative.py
@@ -1,26 +1,10 @@
+def factorial(n):
+    fact = 1
+    for num in range(2, n + 1):
+        fact *= num
+    return fact
 
-def fact_iterative(n):
-    facto=1
-    if(n==0):
-        # print("Factorial of 0 is 1")
-        return 1
-    else:
-        for i in range(1,n+1):
-            facto=facto*i
-        return facto
-        # print("Factorial of ",n," is :",facto )
-        
-def fact_recursive(n):
-    
-    if(n==0):
-        # print("Factorial of 0 is 1")
-        return 1
-    else:
-         
-        return n*fact_recursive(n-1)
-        # print("Factorial of ",n," is :",facto )
-
-print(fact_iterative(3))
-print(fact_iterative(3))
+print(factorial(3))
+print(factorial(4))
 
 
